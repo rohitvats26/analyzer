77659bbdb93d0c5cfd0c18e0c007f64194776483
Rohit Sharma <rohit.sharma09@nagarro.com>
2026-04-27 08:27:40 +0530

    first commit 41

--- src/main/java/com/impact/analyzer/controller/WebhookController.java
+++ src/main/java/com/impact/analyzer/controller/WebhookController.java
@@ -1,7 +1,5 @@
 package com.impact.analyzer.controller;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.impact.analyzer.model.DependencyGraph;
